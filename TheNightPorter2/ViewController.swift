18980d706ecbc11612f598e60012be2376b20011
Yang Lu <leon@Yangs-MBP.home>
2018-02-23 09:02:53 -0800

    Demo example: Diagnosing View Hierachies
    Module: 9.2

--- TheNightPorter2/ViewController.swift
+++ TheNightPorter2/ViewController.swift
@@ -8,18 +8,103 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // create task arrays
+    let dailyTasks = ["Check all windows",
+                      "Check all doors",
+                      "Check temperature of freezer",
+                      "Check the mailbox at the end of the lane",
+                      "Empty trash containers",
+                      "If freezing, check water pipes outside"]
+    
+    let weeklyTasks = ["Check inside all unoccupied cabins",
+                       "Run all faucets for 30 seconds",
+                       "Walk the perimeter of property",
+                       "Arrange for dumpster pickup"]
+    
+    let twoWeekTasks = ["Run tests on security alarm",
+                        "Check all motion detectors",
+                        "Test smoke alarms"]
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        tableView.backgroundColor = UIColor.clear
+        
+        return 3
+    }
+    
+    @IBAction func toggleDarkSwitch(_ sender: Any) {
+        let mySwitch = sender as! UISwitch
+        
+        if mySwitch.isOn{
+            view.backgroundColor = UIColor.darkGray
+        }else{
+            view.backgroundColor = UIColor.white
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section{
+        case 0:
+            return dailyTasks.count
+        case 1:
+            return weeklyTasks.count
+        case 2:
+            return twoWeekTasks.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        var currentTask: String
+        switch indexPath.section{
+        case 0:
+            currentTask = dailyTasks[indexPath.row]
+        case 1:
+            currentTask = weeklyTasks[indexPath.row]
+        case 2:
+            currentTask = twoWeekTasks[indexPath.row]
+        default:
+            currentTask = ""
+        }
+        
+        cell.textLabel!.text = currentTask
+        cell.backgroundColor = UIColor.clear
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
 
+        switch section{
+        case 0:
+            return "Daily Tasks"
+        case 1:
+            return "Weekly Tasks"
+        case 2:
+            return "Every two weeks"
+        default:
+            return "This should not be here"
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You selected row: \(indexPath.row) in section: \(indexPath.section)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
+
 
